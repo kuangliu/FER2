3c1f98745f983e7a8aee5f2eaa9e665e595e618b
Kuang Liu <kuang.liu@hotmail.com>
2016-02-18 19:26:05 +0800

    update crop_image to crop at every possible position

--- crop_image.m
+++ crop_image.m
@@ -6,7 +6,7 @@ function cropped = crop_image(X, h, w, n)
 %       - 3D volumn: RGB image [H,W,C], C=3
 %       - 4D volumn: N images [H,W,C,N] 
 %   - h, w: cropped size [h, w]
-%   - n: crop times
+%   - n: crop times, if n==0, we return every possible crop
 % Output:
 %   - cropped: cropped volumn sized [h, w, C, n*N]
 
@@ -16,11 +16,27 @@ deltaX = W-w;
 deltaY = H-h;
 assert(deltaX>=0 && deltaY>=0);
 
-cropped = zeros(h,w,C,n*N,'like',X);
-for i = 1:n
-    x = randi(deltaX);
-    y = randi(deltaY);
-    cropped(:,:,:,i) = X(y:y+h-1, x:x+w-1, :, :); 
-    % figure; imshow(cropped(:,:,:,i));
+if n == 0
+    % crop every possible position
+    n = (deltaX+1)*(deltaY+1);
+    cropped = zeros(h,w,C,n*N,'like',X);
+    fprintf('you choose iteration crop mode, you will get %dX more data.\n', n);
+    i = 1;
+    for y = 1:deltaY+1
+        for x = 1:deltaX+1
+            cropped(:,:,:,i) = X(y:y+h-1, x:x+w-1, :, :); 
+            i = i + 1;
+        end
+    end
+else
+    % random crop
+    cropped = zeros(h,w,C,n*N,'like',X);
+    for i = 1:n
+        x = randi(deltaX);
+        y = randi(deltaY);
+        cropped(:,:,:,i) = X(y:y+h-1, x:x+w-1, :, :); 
+        % figure; imshow(cropped(:,:,:,i));
+    end
 end
+
 
