bd29d4dd8ced300869ff467e1ee544494e3eb432
Mrliukuang <kuang.liu@hotmail.com>
2016-02-19 13:12:41 +0800

    fix 4D crop bug

--- crop_image.m
+++ crop_image.m
@@ -1,12 +1,12 @@
-function cropped = crop_image(X, h, w, n)
+function cropped = crop_image(X, h, w, varargin)
 %CROP_IMAGE Crop the given volumn X to generate more training data
 % Inputs:
 %   - X: input volumn, could be
 %       - 2D volumn: gray image [H,W]
 %       - 3D volumn: RGB image [H,W,C], C=3
-%       - 4D volumn: N images [H,W,C,N] 
+%       - 4D volumn: N images [H,W,C,N]
 %   - h, w: cropped size [h, w]
-%   - n: crop times, if n==0, we return every possible crop
+%   - varargin: crop times, if n==0, we return every possible crop
 % Output:
 %   - cropped: cropped volumn sized [h, w, C, n*N]
 
@@ -16,27 +16,27 @@ deltaX = W-w;
 deltaY = H-h;
 assert(deltaX>=0 && deltaY>=0);
 
-if n == 0
+if nargin == 3 || isempty(varargin)
     % crop every possible position
     n = (deltaX+1)*(deltaY+1);
     cropped = zeros(h,w,C,n*N,'like',X);
     fprintf('you choose iteration crop mode, you will get %dX more data.\n', n);
     i = 1;
-    for y = 1:deltaY+1
-        for x = 1:deltaX+1
-            cropped(:,:,:,i) = X(y:y+h-1, x:x+w-1, :, :); 
+    for x = 1:deltaX+1
+        for y = 1:deltaY+1
+            cropped(:,:,:,(i-1)*N+1:i*N) = X(y:y+h-1, x:x+w-1, :, :);
             i = i + 1;
         end
     end
 else
     % random crop
+    n = varargin{1};
     cropped = zeros(h,w,C,n*N,'like',X);
     for i = 1:n
-        x = randi(deltaX);
-        y = randi(deltaY);
-        cropped(:,:,:,i) = X(y:y+h-1, x:x+w-1, :, :); 
+        x = randi(deltaX+1);
+        y = randi(deltaY+1);
+        cropped(:,:,:,(i-1)*N+1:i*N) = X(y:y+h-1, x:x+w-1, :, :);
+        
         % figure; imshow(cropped(:,:,:,i));
     end
 end
-
-
